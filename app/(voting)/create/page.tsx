682f44431a89d8cd57b4c5c3bc4b16c0990a3899
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-04-26 11:12:46 +0200

    C:/Program Files/Git/create

--- app/(voting)/create/page.tsx
+++ app/(voting)/create/page.tsx
@@ -1,3 +1,133 @@
-export default function Page() {
-    return <div>Page</div>;
+"use client";
+
+import { Input } from "@/components/ui/Input";
+import { Button } from "@/components/ui/Button";
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/Select";
+import { IVotingInputs } from "@/types";
+import { FormEvent, KeyboardEvent, useState } from "react";
+
+export default function Page(): JSX.Element {
+    const [title, setTitle] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [img, setImg] = useState<string>("");
+    const [endtime, setEndtime] = useState<string>("");
+    const [isPrivate, setIsPrivate] = useState(false);
+    const [option1, setOption1] = useState<string>("");
+    const [option2, setOption2] = useState<string>("");
+    const [allowList, setAllowList] = useState<string[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [selectedUser, setSelectedUser] = useState<string>("");
+
+    const inputs: IVotingInputs[] = [
+        { type: "text", name: "title", placeholder: "Title" },
+        { type: "text", name: "description", placeholder: "Description" },
+        { type: "url", name: "img", placeholder: "Image" },
+        { type: "time", name: "endtime", placeholder: "End Time" },
+    ];
+    const selectOptions = { options: ["Public", "Private"], placeholder: "Visibility" };
+    const optionsInputs: IVotingInputs[] = [
+        { type: "text", name: "option1", placeholder: "First Option" },
+        { type: "text", name: "option2", placeholder: "Second Options" },
+    ];
+
+    const handleSubmit = async () => {
+        try {
+            setLoading(true);
+            console.log({ title, description, img, endtime, isPrivate, option1, option2, allowList });
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <main className="p-5 flex flex-col items-start justify-start gap-y-3">
+            {inputs.map((input: IVotingInputs) => (
+                <Input
+                    key={input.name}
+                    {...input}
+                    className="font-semibold"
+                    onChange={(e: FormEvent<HTMLInputElement>) => {
+                        switch (input.name) {
+                            case "title":
+                                setTitle(e.currentTarget.value);
+                                break;
+                            case "description":
+                                setDescription(e.currentTarget.value);
+                                break;
+                            case "img":
+                                setImg(e.currentTarget.value);
+                                break;
+                            case "endtime":
+                                setEndtime(e.currentTarget.value);
+                                break;
+                        }
+                    }}
+                />
+            ))}
+            <section>
+                <Select>
+                    <SelectTrigger className="w-full">
+                        <SelectValue placeholder={selectOptions.placeholder} />
+                    </SelectTrigger>
+                    <SelectContent className="w-full">
+                        {selectOptions.options.map(
+                            (option: string): JSX.Element => (
+                                <SelectItem
+                                    className="w-full"
+                                    key={option}
+                                    value={option}
+                                    onChange={() => setIsPrivate(option === "Private" ? true : false)}
+                                >
+                                    {option}
+                                </SelectItem>
+                            ),
+                        )}
+                    </SelectContent>
+                </Select>
+                {isPrivate && (
+                    <Input
+                        type="text"
+                        name="allowList"
+                        placeholder="Submit user on space or enter"
+                        value={selectedUser}
+                        onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => {
+                            if ((e.key === "Enter" || "Space") && selectedUser) {
+                                setAllowList([...allowList, selectedUser]);
+                                setSelectedUser("");
+                            }
+                        }}
+                        onChange={() => setSelectedUser(selectedUser)}
+                    />
+                )}
+            </section>
+
+            <section className="flex flex-col items-start justify-between">
+                <h1>Choose an options</h1>
+                <div className="flex items-center justify-between gap-x-3">
+                    {optionsInputs.map((input: IVotingInputs) => (
+                        <Input
+                            key={input.name}
+                            {...input}
+                            onChange={(e: FormEvent<HTMLInputElement>) => {
+                                switch (input.name) {
+                                    case "option1":
+                                        setOption1(e.currentTarget.value);
+                                        break;
+                                    case "option2":
+                                        setOption2(e.currentTarget.value);
+                                        break;
+                                }
+                            }}
+                        />
+                    ))}
+                </div>
+            </section>
+
+            <Button type="submit" onClick={handleSubmit} disabled={loading} variant="secondary" className="w-full">
+                Create
+            </Button>
+        </main>
+    );
 }
--- components/constant/CategoriesNavbar.tsx
+++ components/constant/CategoriesNavbar.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import clsx from "clsx";
 import { ScrollArea, ScrollBar } from "../ui/ScrollArea";
+import { useState } from "react";
 
 /**
  * @param user id to make suited categories
  * @returns
  */
 export default function CategoriesNavbar({ userId }: { userId: string }): JSX.Element {
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
     const categories = [
         "All",
         "Trending",
@@ -19,14 +24,26 @@ export default function CategoriesNavbar({ userId }: { userId: string }): JSX.El
         "Sports",
     ];
 
+    const handleClick = (category: string) => {
+        if (selectedCategories.includes(category)) {
+            setSelectedCategories(selectedCategories.filter((c) => c !== category));
+        } else {
+            setSelectedCategories([...selectedCategories, category]);
+        }
+    };
+
     return (
         <div className="w-full overflow-x-auto">
             <ScrollArea className="whitespace-nowrap">
-                <div className="flex space-x-4">
+                <div className="flex space-x-4 mb-2">
                     {categories.map((category) => (
                         <button
                             key={category}
-                            className="text-sm text-white hover:bg-zinc-800 px-3 py-2 rounded-full cursor-pointer"
+                            onClick={() => handleClick(category)}
+                            className={clsx(
+                                "text-sm text-white hover:bg-zinc-800 transition-all duration-300 ease-in-out px-3 py-2 rounded-full cursor-pointer",
+                                selectedCategories.includes(category) ? "bg-zinc-700" : "bg-black",
+                            )}
                         >
                             {category}
                         </button>
