5b9eef09dd76a3a7eb720dfdebae83f00d5730b5
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-05-10 17:59:36 +0300

    ctr c ctr v

---
+++ utils/scaffol-eth/contractsData.ts
@@ -0,0 +1,7 @@
+import scaffoldConfig from "@/scaffold.config";
+import { contracts } from "./contract";
+
+export function getAllContracts() {
+    const contractsData = contracts?.[scaffoldConfig.targetNetworks[0].id];
+    return contractsData ? contractsData : {};
+}
