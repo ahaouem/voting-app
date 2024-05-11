5c5f408eb9bf502809a903ff111f28de6053915e
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-05-11 13:33:47 +0300

    revert changes #2

--- components/constant/Providers.tsx
+++ components/constant/Providers.tsx
@@ -2,24 +2,20 @@
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
-import { WagmiProvider } from "wagmi";
-import { config } from "../../config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function Providers({ children }: Readonly<{ children: React.ReactNode }>): JSX.Element {
     const queryClient: QueryClient = new QueryClient();
 
     return (
-        <WagmiProvider config={config}>
-            <QueryClientProvider client={queryClient}>
-                <ClerkProvider
-                    appearance={{
-                        baseTheme: dark,
-                    }}
-                >
-                    {children}
-                </ClerkProvider>
-            </QueryClientProvider>
-        </WagmiProvider>
+        <QueryClientProvider client={queryClient}>
+            <ClerkProvider
+                appearance={{
+                    baseTheme: dark,
+                }}
+            >
+                {children}
+            </ClerkProvider>
+        </QueryClientProvider>
     );
 }
