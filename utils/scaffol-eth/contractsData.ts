977b051b1f9512013cf93b0694560b16d2b1e234
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-05-11 13:26:59 +0300

    revert changes

--- utils/scaffol-eth/contractsData.ts
+++
@@ -1,7 +0,0 @@
-import scaffoldConfig from "@/scaffold.config";
-import { contracts } from "./contract";
-
-export function getAllContracts() {
-    const contractsData = contracts?.[scaffoldConfig.targetNetworks[0].id];
-    return contractsData ? contractsData : {};
-}
