f9ca261baf87c602e5fa7028ab92251ba41716b5
Aleksander Haouem <4leeeksha@gmail.com>
2024-05-19 12:26:06 +0200

    trying to fix

--- components/vote/VotingsSection.tsx
+++ components/vote/VotingsSection.tsx
@@ -1,38 +1,55 @@
 "use client";
 
-import { mockVotings } from "@/lib/mocks";
 import Voting from "./Voting";
 import CreateVotingButton from "../constant/CreateVotingButton";
 import { useStateContext } from "@/context/Context";
 import { useEffect, useState } from "react";
+import { IVoting } from "@/types"; // Ensure this path is correct
+
+type VotingProps = {
+    user_id: string;
+    voting_id: string;
+    userPic: string;
+    username: string;
+    timestamp: Date;
+    title: string;
+    description: string;
+    options: string[];
+};
 
 export default function VotingsSection() {
-    const [polls, setPolls] = useState([]);
+    const [polls, setPolls] = useState<IVoting[]>([]);
     const { getAllPolls } = useStateContext();
 
     useEffect(() => {
         const fetchPolls = async () => {
             const data = await getAllPolls();
 
-            setPolls(data);
+            if (data) {
+                setPolls(data);
+            }
         };
 
         fetchPolls();
-    }, []);
+    }, [getAllPolls]);
+
+    const transformVotingData = (voting: IVoting): VotingProps => ({
+        user_id: voting.owner,
+        voting_id: voting.title,
+        userPic: voting.image,
+        username: voting.owner, 
+        timestamp: new Date(voting.endTime),
+        title: voting.title,
+        description: voting.description,
+        options: voting.choices,
+    });
 
     return (
         <div className="p-5 flex flex-col items-start justify-start gap-y-5 w-screen h-full">
-            {polls.map((voting) => (
+            {polls.map((voting: IVoting) => (
                 <Voting
-                    key={voting.voting_id}
-                    user_id={voting.id}
-                    voting_id={voting.voting_id}
-                    userPic={voting.userPic}
-                    username={voting.username}
-                    timestamp={voting.timestamp}
-                    title={voting.title}
-                    description={voting.description}
-                    options={voting.options}
+                    key={voting.owner + voting.title} 
+                    {...transformVotingData(voting)}
                 />
             ))}
             <CreateVotingButton />
